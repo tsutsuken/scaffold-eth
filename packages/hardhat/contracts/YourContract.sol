165717ff9d58cc1a069de4e93ef6a7a54615e8f7
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-01-11 15:39:08 +0900

    feat: add getLatestEthPrice

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
@@ -9,11 +10,56 @@ import "hardhat/console.sol";
 contract YourContract {
     event SetPurpose(address sender, string purpose);
 
+    AggregatorV3Interface internal priceFeed;
     string public purpose = "Building Unstoppable Apps!!!";
     uint256 public count = 0;
 
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     */
     constructor() {
-        // what should we do on deploy?
+        priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestEthPrice()
+        public
+        view
+        returns (
+            string memory,
+            uint8,
+            uint80,
+            int256,
+            uint256,
+            uint256,
+            uint80
+        )
+    {
+        string memory description = priceFeed.description();
+        uint8 decimals = priceFeed.decimals();
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+
+        return (
+            description,
+            decimals,
+            roundID,
+            price,
+            startedAt,
+            timeStamp,
+            answeredInRound
+        );
     }
 
     function setPurpose(string memory newPurpose) public {
