89e6da8b4f206e3b6ad7704336b652c8d180065e
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-01-18 13:48:56 +0900

    feat: change YourCollectible to ERC721URIStorage

--- packages/hardhat/scripts/mint.js
+++ packages/hardhat/scripts/mint.js
@@ -1,5 +1,13 @@
 /* eslint no-use-before-define: "warn" */
 
+const ipfsAPI = require("ipfs-http-client");
+
+const ipfs = ipfsAPI({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https",
+});
+
 const delayMS = 1000; // sometimes xDAI needs a 6000ms break lol 😅
 
 const main = async () => {
@@ -11,8 +19,31 @@ const main = async () => {
   const { deployer } = await getNamedAccounts();
   const yourCollectible = await ethers.getContract("YourCollectible", deployer);
 
-  console.log("Minting buffalo...");
-  await yourCollectible.mint(toAddress, {
+  const buffalo = {
+    description: "It's actually a bison?",
+    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
+    image: "https://austingriffith.com/images/paintings/buffalo.jpg",
+    name: "Buffalo",
+    attributes: [
+      {
+        trait_type: "BackgroundColor",
+        value: "green",
+      },
+      {
+        trait_type: "Eyes",
+        value: "googly",
+      },
+      {
+        trait_type: "Stamina",
+        value: 42,
+      },
+    ],
+  };
+  console.log("Uploading buffalo...");
+  const uploaded = await ipfs.add(JSON.stringify(buffalo));
+
+  console.log("Minting buffalo with IPFS hash (" + uploaded.path + ")");
+  await yourCollectible.safeMint(toAddress, uploaded.path, {
     gasLimit: 400000,
   });
 
