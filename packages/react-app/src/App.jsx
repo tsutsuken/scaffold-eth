8de260bb5711be223ad1d6f6f9f45fe01a30b911
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-01-19 13:56:19 +0900

    feat: display Davatars of influencer

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -270,6 +270,17 @@ function App(props) {
   };
   // fetchEnsInfo("0x983110309620D911731Ac0932219af06091b6744");
 
+  const influencerAddressList = [
+    "0x983110309620D911731Ac0932219af06091b6744",
+    "0xE545c0D41c622bCCCc8f103c0607D042A2242350",
+    "0x5A384227B65FA093DEC03Ec34e111Db80A040615",
+    "0xa4c8d9e4ec5f2831701a81389465498b83f9457d",
+    "0x961d2C942aECE305A9911652E1fb303A1e5D4902",
+    "0xcEf871B113d2bB5d3FA5DC96d2D288dd38082AAE",
+    "0x9297A132AF2A1481441AB8dc1Ce6e243d879eaFD",
+    "0x5b0f8955658cB26f29695A00b36E1fE0259D4e0A",
+  ];
+
   //
   // 🧫 DEBUG 👨🏻‍🔬
   //
@@ -358,12 +369,11 @@ function App(props) {
       >
         fetchEnsInfo
       </Button>
-      <Davatar
-        size={120}
-        provider={mainnetProvider}
-        address={"0x983110309620D911731Ac0932219af06091b6744"}
-        style={{ borderRadius: 0 }}
-      />
+      {influencerAddressList.map(influencerAddress => {
+        return (
+          <Davatar size={120} provider={mainnetProvider} address={influencerAddress} style={{ borderRadius: 0 }} />
+        );
+      })}
       <List
         bordered
         dataSource={yourCollectibles}
